cc5935f0c8fcae0158bc63995d6905d49e71834b
Denys7717 <denys.rdbua@gmail.com>
2023-07-08 15:08:29 +0300

    update2
    update2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryElem = document.querySelector(".gallery");
+galleryElem.addEventListener("click", showImg);
+
+function showImg(event) {
+  event.preventDefault();
+
+  if (event.target.className === "gallery__image") {
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+    galleryElem.addEventListener("keydown", (evt) => {
+      if (evt.code === "Escape") instance.close();
+    });
+  }
+}
+
+const imgListArr = galleryItems.map(
+  (image) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+);
+const template = imgListArr.join("");
+galleryElem.insertAdjacentHTML("beforeend", template);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryElem = document.querySelector(".gallery");
+galleryElem.addEventListener("click", (event) => event.preventDefault());
+
+const imgListArr = galleryItems.map(
+  (image) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+);
+const template = imgListArr.join("");
+galleryElem.insertAdjacentHTML("beforeend", template);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
